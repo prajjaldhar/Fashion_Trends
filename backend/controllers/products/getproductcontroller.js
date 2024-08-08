c0ff86d4f6efb14b358ead9a56bd3d396971dd8e
prajjal <prajjaldhar41@gmail.com>
2024-08-08 12:25:21 +0530

    adding more get requests

--- backend/controllers/products/getproductcontroller.js
+++ backend/controllers/products/getproductcontroller.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const ProductCollection = require("../../models/ProductSchema");
 const getproductcontroller = async (req, res) => {
   try {
-    const { category, name, subcategory } = req.params;
+    const { id, category, name, subcategory } = req.params;
     let products;
     if (category) {
       const searchcategory = category.toLowerCase();
@@ -10,20 +10,40 @@ const getproductcontroller = async (req, res) => {
         category: { $regex: new RegExp(searchcategory, "i") },
       });
     } else if (name) {
-      const searchname = name.toLowerCasease();
+      const searchname = name.toLowerCase();
       products = await ProductCollection.find({
         name: { $regex: new RegExp(searchname, "i") },
       });
+    } else if (req.path.includes("/random")) {
+      products = await ProductCollection.aggregate([
+        {
+          $sample: {
+            size: 9,
+          },
+        },
+      ]);
+    } else if (req.path.includes("/top-rated")) {
+      products = await ProductCollection.find().sort({ rating: -1 }).limit(9);
+    } else if (req.path.includes("/lowtohigh")) {
+      products = await ProductCollection.find().sort({ new_price: 1 }).limit(9);
+    } else if (req.path.includes("/hightolow")) {
+      products = await ProductCollection.find()
+        .sort({ new_price: -1 })
+        .limit(9);
     } else if (subcategory) {
       const searchsubcategory = subcategory.toLowerCase();
       products = await ProductCollection.find({
         sub_category: { $regex: new RegExp(searchsubcategory, "i") },
       });
+    } else if (id) {
+      products = await ProductCollection.find({
+        _id: id,
+      });
     } else {
       products = await ProductCollection.find();
       console.log(`Product fetched successfully`);
     }
-    if (!products || products.length === 0) n 
+    if (!products || products.length === 0)
       return res.status(404).send({ message: "Product not found" });
     res.status(200).send(products);
   } catch (error) {
